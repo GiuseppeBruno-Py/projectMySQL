0ed22859734284be23a424d7e923590a48ea42e0
Giuseppe Bruno <91219935+GiuseppeBruno-Py@users.noreply.github.com>
2022-03-29 17:23:45 -0300

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import MovieList from './components/MovieList';
+
+const App = () => {
+  return (
+    <>
+      <h1>Controle de séries e filmes</h1>
+      <MovieList />
+    </>
+  )
+}
+
+export default App;
